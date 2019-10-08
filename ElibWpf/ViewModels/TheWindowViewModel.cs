2f9845c92ea8b56273402feaf1250a858b8505ca
Luka <nessossin@gmail.com>
2019-10-08 13:08:41 +0200

    wpf initial work

---
+++ ElibWpf/ViewModels/TheWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace ElibWpf.ViewModels
+{
+    public class TheWindowViewModel: ViewModelBase
+    {
+        private readonly Stack<object> viewModelHistory = new Stack<object>();
+        private object currentControl;
+
+        public object CurrentControl
+        {
+            get
+            {
+                return this.currentControl;
+            }
+
+            internal set
+            {
+                viewModelHistory.Push(value);
+                Set("CurrentControl", ref currentControl, value);
+            }
+        }
+
+        public ICommand GoBack { get => new RelayCommand(GoToPreviousViewModel); }
+
+        public void GoToPreviousViewModel()
+        {
+            if (viewModelHistory.Count > 1)
+                viewModelHistory.Pop();
+            this.CurrentControl = viewModelHistory.Peek();
+        }
+    }
+}
---
+++ ElibWpf/Views/TheWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ElibWpf.ViewModels;
+using System.Windows;
+
+namespace ElibWpf.Views
+{
+    /// <summary>
+    /// Interaction logic for TheWindow.xaml
+    /// </summary>
+    public partial class TheWindow : Window
+    {
+        public TheWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new TheWindowViewModel();
+        }
+    }
+}
