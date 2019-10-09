9fb97a02f27c5de0544fdb4ea4e61c66b6d263c3
Luka <nessossin@gmail.com>
2019-10-09 16:28:47 +0200

    db file, some comments and test assert added

--- DatabaseTests/Tests.cs
+++ DatabaseTests/Tests.cs
@@ -6,12 +6,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DatabaseTests
 {
     [TestClass]
-    public class UnitTest1
+    public class Tests
     {
         [TestMethod]
         public void TestMethod1()
         {
             ElibContext context = new ElibContext(@"C:\Users\luka.budrak\Desktop\new_elib-test.db");
+
+            // EntityFramework WILL NOT get items from other tables if not specifically asked to do so
+            // If you want it to pull something as a graph, you need to use Include("NameOfPropertyToInclude");
+            // See examples:
             Author first = context.Authors.Include("Books").FirstOrDefault();
             UserCollection collection = context.UserCollections.Include("Books").FirstOrDefault();
             Book mybook = context.Books
@@ -20,6 +24,12 @@ namespace DatabaseTests
                 .Include("Files")
                 .Include("Quotes")
                 .FirstOrDefault();
+            Assert.IsNotNull(first.Books);
+            Assert.IsNotNull(collection.Books);
+            Assert.IsNotNull(mybook.Series);
+            Assert.IsNotNull(mybook.Authors);
+            Assert.IsNotNull(mybook.Files);
+            Assert.IsNotNull(mybook.Quotes);
         }
     }
 }
