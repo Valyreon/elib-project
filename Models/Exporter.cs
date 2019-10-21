74960a0e37dab4e36543d630697ff8f51ae63aab
gygasync <gygasync@DESKTOP-F39QI5N>
2019-10-21 22:40:06 +0200

    Add ExportBook method to Exporter

--- Models/Exporter.cs
+++ Models/Exporter.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DataLayer;
+using Domain;
+using Models.Options;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,24 @@ namespace Models
 {
     public class Exporter
     {
+        private readonly ElibContext database;
+
+        public Exporter()
+        {
+            database = new ElibContext(ApplicationSettings.GetInstance().DatabasePath);
+        }
+
+        public void ExportBook(Book book, string path, ExporterOptions.Format format)
+        {
+            byte[] binaryData = book.Files.Where(x => x.Format == ExporterOptions.GetFormat(format)).FirstOrDefault().RawContent;
+
+            if (binaryData == null)
+                throw new FileNotFoundException();
+
+            using(FileStream fs = File.Create(path))
+            {
+                fs.Write(binaryData, 0, binaryData.Length);
+            }
+        }
     }
 }
--- Models/Options/ExporterOptions.cs
+++ Models/Options/ExporterOptions.cs
@@ -8,5 +8,23 @@ namespace Models.Options
 {
     public class ExporterOptions
     {
+        public enum Format
+        {
+            Epub,
+            Mobi
+        }
+
+        public static string GetFormat(Format format)
+        {
+            switch(format)
+            {
+                case Format.Epub:
+                    return ".epub";
+                case Format.Mobi:
+                    return ".mobi";
+                default:
+                    return "";
+            }
+        }
     }
 }
