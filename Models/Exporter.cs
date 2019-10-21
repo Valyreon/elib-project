d5a9f96f6d614653eb7c207dbb5423113c1445a2
valyreon <nessossin@gmail.com>
2019-10-21 22:11:17 +0200

    Revert "Merge branch 'feature-exporter' of https://github.com/Valyreon/elib-project into feature-exporter"
    This reverts commit 0d46a2924ae88b8d52467182e034eedc38f4faea, reversing
    changes made to aa58b003589c0b88f19ea522d09c4e0cac2fc78b.

--- Models/Exporter.cs
+++ Models/Exporter.cs
@@ -1,9 +1,5 @@
-﻿using DataLayer;
-using Domain;
-using Models.Options;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,24 +8,5 @@ namespace Models
 {
     public class Exporter
     {
-        private readonly ElibContext database;
-
-        public Exporter()
-        {
-            database = new ElibContext(ApplicationSettings.GetInstance().DatabasePath);
-        }
-
-        public static void ExportBook(Book book, string path, ExporterOptions.Format format)
-        {
-            byte[] outputBinary = book.Files.Where(x => x.Format == ExporterOptions.GetFormat(format)).FirstOrDefault()?.RawContent;
-
-            if (outputBinary == null)
-                throw new FileNotFoundException();
-
-            using (FileStream fs = File.Create(path))
-            {
-                fs.Write(outputBinary, 0, outputBinary.Length);
-            }
-        }
     }
 }
