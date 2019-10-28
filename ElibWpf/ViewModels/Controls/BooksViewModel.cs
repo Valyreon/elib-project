7e858aabbe40602cc049d989a68588339869e1a4
Luka <nessossin@gmail.com>
2019-10-28 14:27:41 +0100

    started work on books tab

--- ElibWpf/ViewModels/Controls/BooksViewModel.cs
+++ ElibWpf/ViewModels/Controls/BooksViewModel.cs
@@ -1,4 +1,7 @@
-﻿using GalaSoft.MvvmLight;
+﻿using DataLayer;
+using Domain;
+using GalaSoft.MvvmLight;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,15 @@ namespace ElibWpf.ViewModels.Controls
         {
             get => _caption;
             set => Set(ref _caption, value);
+        }
+
+        public List<UserCollection> Collections { get; set; }// = new ElibContext(ApplicationSettings.GetInstance().DatabasePath).UserCollections.ToList();
+
+        private bool _isPaneOpen = true;
+        public bool IsPaneOpen
+        {
+            get => _isPaneOpen;
+            set => Set(ref _isPaneOpen, true);
         }
     }
 }
