aeef0eacd8b1a816a9b082248af325e4e5739ebe
Luka <nessossin@gmail.com>
2019-10-29 09:17:42 +0100

    xaml formatting

---
+++ ElibWpf/Components/CustomSearchBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ElibWpf.Components
+{
+    /// <summary>
+    /// Interaction logic for CustomSearchBox.xaml
+    /// </summary>
+    public partial class CustomSearchBox : UserControl
+    {
+        public CustomSearchBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
