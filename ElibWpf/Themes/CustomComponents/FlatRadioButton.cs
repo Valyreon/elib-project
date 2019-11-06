0a713566083e5412af0068ef446059ee253f8f5c
Luka <nessossin@gmail.com>
2019-11-06 16:40:43 +0100

    some reorganizing

---
+++ ElibWpf/Themes/CustomComponents/FlatRadioButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ElibWpf.CustomComponents
+{
+    public class FlatRadioButton : RadioButton
+    {
+        static FlatRadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatRadioButton), new FrameworkPropertyMetadata(typeof(FlatRadioButton)));
+        }
+    }
+}
