43af8005458799f4961275a06425e499180ae596
valyreon <nessossin@gmail.com>
2020-04-24 02:12:15 +0200

    Tooltips are displayed on hover for title, authors or series if the string is too long to show in UI

---
+++ ElibWpf/Converters/BookTileConverters/TitleToVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ElibWpf.Converters.BookTileConverters
+{
+    /// <summary>
+    /// Used to specify is tooltip enabled based on string length.
+    /// </summary>
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value is string str && parameter is string maxLengthStr)
+            {
+                try
+                {
+                    int maxLength = int.Parse(maxLengthStr);
+                    if (str.Length > maxLength)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch(Exception)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
